63c828c70068fcf454969471e85a692d42d7c2ec
Ankesh-287 <ankeshpatil287@gmail.com>
2024-12-11 12:28:45 +0530

    Change routes index into Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import {createBrowserRouter} from "react-router-dom";
-import Login from '../Pages/Login'
-import Register from '../Pages/Register'
-import HomeLayout from "../Layouts/HomeLayout";
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import HomeLayout from "./Layouts/HomeLayout";
 // import Network from "../Pages/Network.jsx"
-import Jobs from "../Pages/Jobs.jsx";
-import ProfileLayout from "../Layouts/ProfileLayout";
-import Messaging from '../Pages/Messaging.jsx'
-import ConnectionLayout from "../Layouts/ConnectionLayout.jsx";
+import Jobs from "./Pages/Jobs.jsx";
+import ProfileLayout from "./Layouts/ProfileLayout";
+import Messaging from './Pages/Messaging.jsx'
+import ConnectionLayout from "./Layouts/ConnectionLayout.jsx";
 
 export const router = createBrowserRouter([
     {
